bc7ef34987a9f22751cdaa466e352fccf17edf3b
lanthian <lanthian@student.unimelb.edu.au>
2024-07-26 12:07:20 +1000

    P70: Faster phi generation, but still too slow

--- problems/070/main.py
+++ problems/070/main.py
@@ -16,44 +16,30 @@ https://projecteuler.net/problem=70
 __author__ = "Liam Anthian"
 
 # --- Imports ---
-from common.iters import permutation
-from common.primes import prime_factors, prime_sieve
+from operator import mul
+from common.iters import operate_list, permutation
+from common.primes import tupled_prime_factors, prime_factors, prime_sieve
 
 # --- Conditions of the problem ---
 LIMIT = 10**7           # Exclusive
 
 
+def phi(n: int) -> int:
+    """Returns the totient / phi function value for number `n`."""
+    paired_facs = tupled_prime_factors(n)
+    return operate_list(1,[p**(i-1) * (p-1) for (p,i) in paired_facs],mul)
+
+
 # --- Calculation ---
 def main():
-    primes = prime_sieve(LIMIT)
-    prime_set = set(primes)
-    not_prime = []
-    
-    factors = {}
-    phi = {}
-    
     best_ratio = 1000
     best_n = 1
     for n in range (2, LIMIT):
-        factors[n] = set(prime_factors(n, primes, reduced=True))
-        # If prime, don't need to check factor overlap
-        if n in prime_set: phi[n] = n-1
-       
-        # Otherwise, count up relatively prime lower numbers
-        else:
-            count = 1
-            for i in range(2,n):
-    
-                # Check if any factor overlap
-                for factor in factors[n]:
-                    if factor in factors[i]: break
-                else: count += 1
-            # And update phi(n) accordingly
-            phi[n] = count
+        pn = phi(n)
 
         # Update smallest perm ratio if necessary
-        if permutation(str(n), str(phi[n])):
-            ratio = n / phi[n]
+        if permutation(str(n), str(pn)):
+            ratio = n / pn
             if ratio < best_ratio:
                 print("%s -> %s, %s -> %s" % (best_n, n, best_ratio, ratio))
                 best_ratio = ratio
@@ -61,7 +47,7 @@ def main():
 
     # Hypothesising it'll be better to work backwards from the limit to some 
     #   tipping point, rather than forwards
-
+    
 
     # --- Output ---
     print(best_n)

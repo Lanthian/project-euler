ce8b88ff181087168a43d8daae1262b30ab8838e
lanthian <lanthian@student.unimelb.edu.au>
2024-08-02 10:15:43 +1000

    P67: Solved

--- problems/067/main.py
+++ problems/067/main.py
@@ -22,35 +22,11 @@ __author__ = "Liam Anthian"
 # --- Imports ---
 from operator import add
 from common.files import easy_open
+from common.valwrap import ValWrap
 
 # --- Conditions of the problem ---
 FILE = "triangle.txt"
 DELIM = " "
-
-
-def sub_triangle(triangle: list[list], index: int) -> list[list]:
-    """Takes a triangle `triangle` (list of lists of items, each row larger than
-    the prior) and returns a subtriangle with root at `index` of second row."""
-    return [row[index:i+index+1] for i,row in enumerate(triangle[1:])]
-
-def max_tri_recurse(triangle: list[list], op, base: ...) -> ...:
-    """Via brute force (testing each path) and maximimising at each tree node,
-    finds the maximum possible value of a binary operation `op` enacted across a
-    triangular list of lists, `triangle`. Returns this value."""
-    rows = len(triangle)
-    # Check if triangle == [] initially...
-    if rows == 0: return base
-    # or if at the bottom of triangle
-    if rows == 1: return max(triangle[0])
-
-    # Otherwise, recurse and operate
-    sub_maxes = []
-    for i in range(len(triangle[1])):
-        # Generate sub triangle and find it's maximum
-        sub_tri = sub_triangle(triangle, i)
-        sub_maxes.append(max_tri_recurse(sub_tri, op, base))
-
-    return op(max(sub_maxes), triangle[0][0])
 
 
 # --- Calculation ---
@@ -60,7 +36,33 @@ def main():
     triangle = [[int(n) for n in s.split(DELIM)] for s in fp.readlines()]
     fp.close()
 
+    # Build a tree of greatest (largest) path to each cell from triangle top
+    tree = {(0,0): ValWrap(triangle[0][0], [0])}
+
+    # Working down the rows, across the columns
+    for r,row in enumerate(triangle[1:], 1):
+        for c,cell in enumerate(row):
+            
+            # Find best parent path to current cell
+            left = (r-1,c-1)
+            right = (r-1,c)
+            # If only left or right exists - has to be parent
+            if left not in tree: parent = tree[right]
+            elif right not in tree: parent = tree[left]
+
+            else:
+                # Otherwise compare possible parent values
+                if tree[left].val >= tree[right].val: parent = tree[left]
+                else: parent = tree[right]
+            
+            # Update this cell's best path
+            tree[(r,c)] = ValWrap(parent.val + cell, parent.item + [c])
+
+    # Find max path
+    last_row = len(triangle) - 1
+    max_path_value = max([wrap.val for (r,_),wrap in tree.items() if r == last_row])
+    
 
     # --- Output ---
-    print(max_tri_recurse(triangle, add, 0))
+    print(max_path_value) # 7,273
     return
